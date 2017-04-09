e6d48e6654e8580aee6b82d9ffa3371119a2bcbb
Jonathan Wight <jwight@mac.com>
2017-04-09 12:04:28 -0700

    Lets get better at testing results of build.

--- testing/test_example_projects.py
+++ testing/test_example_projects.py
@@ -8,36 +8,52 @@ import punic.shshutil as shutil
 
 import tempfile
 import os
+import contextlib
 
 quick_tests_only = bool(int(os.environ.get('QUICK_TEST_ONLY', '0')))
 
-def build(source):
+@contextlib.contextmanager
+def example_work_directory(name):
+    examples_directory = Path(__file__).parent / 'Examples'
 
+    example_path = examples_directory / name
 
-    destination = Path(tempfile.mkdtemp()) / source.name
+    work_path = Path(tempfile.mkdtemp()) / name
+    print(work_path)
 
-    shutil.copytree(source, destination)
+    shutil.copytree(example_path, work_path)
 
-    with work_directory(destination):
-
-        output = runner.check_run('punic resolve')
-        output = runner.check_run('punic update')
-        output = runner.check_run('punic fetch')
-        output = runner.check_run('punic build')
-
-        # assert (Path.cwd() / 'Carthage/Build/Mac/SwiftIO.framework').exists()
-        # assert (Path.cwd() / 'Carthage/Build/Mac/SwiftUtilities.framework').exists()
-        # assert (Path.cwd() / 'Carthage/Build/Mac/SwiftIO.dSYM').exists()
-        # assert (Path.cwd() / 'Carthage/Build/Mac/SwiftUtilities.dSYM').exists()
-        #
-        # output = runner.check_run('punic build')
-        #
-
-examples_directory = Path(__file__).parent / 'Examples'
+    with work_directory(work_path):
+        yield
 
 def test_local_1():
-    build(examples_directory / 'local_1')
+    with example_work_directory('local_1'):
+
+        assert(Path('Cartfile.resolved').exists() == False)
+        output = runner.check_run('punic resolve')
+        assert(Path('Cartfile.resolved').open().read() == 'local "A" "."\n')
+
+        # TODO: This should NOT be created by a resolve.
+        # assert(Path('Carthage/Checkouts').exists() == False)
+        output = runner.check_run('punic fetch')
+        assert(Path('Carthage/Checkouts').is_dir() == True)
+
+        # TODO: This should NOT be created by a fetch.
+        # assert(Path('Carthage/Build').exists() == False)
+        output = runner.check_run('punic build')
+        assert(Path('Carthage/Build/Mac/A.framework').exists() == True)
+        assert(Path('Carthage/Build/Mac/A.dSYM').exists() == True)
 
 def test_rx():
-    build(examples_directory / 'rx')
+    with example_work_directory('rx'):
+        output = runner.check_run('punic update')
+        assert(Path('Carthage/Build/Mac/RxBlocking.framework').exists() == True)
+        assert(Path('Carthage/Build/Mac/RxBlocking.dSYM').exists() == True)
+        assert(Path('Carthage/Build/Mac/RxCocoa.framework').exists() == True)
+        assert(Path('Carthage/Build/Mac/RxCocoa.dSYM').exists() == True)
+        assert(Path('Carthage/Build/Mac/RxSwift.framework').exists() == True)
+        assert(Path('Carthage/Build/Mac/RxSwift.dSYM').exists() == True)
+        assert(Path('Carthage/Build/Mac/RxTest.framework').exists() == True)
+        assert(Path('Carthage/Build/Mac/RxTest.dSYM').exists() == True)
+
 
